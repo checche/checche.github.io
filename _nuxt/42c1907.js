2d5a1f1ae79b3b3b9aa859e01158a12eb33fabfe
checche <checche@users.noreply.github.com>
2021-06-09 11:43:32 +0000

    deploy: 772b05b09bddfe89925840304bb91fb0ca7e440d

---
+++ _nuxt/42c1907.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{275:function(e,n,t){"use strict";t.r(n);var l=t(1).a.extend({}),c=t(45),component=Object(c.a)(l,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("testpage")])}),[],!1,null,null,null);n.default=component.exports}}]);
