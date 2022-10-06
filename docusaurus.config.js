bb29e6b9f50c4d98fb846088e9e501b36c244aee
TakahiroHirata <takakun47@gmail.com>
2022-10-07 08:50:53 +0900

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,15 +16,16 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'checche', // Usually your GitHub org/user name.
+  projectName: 'checche.github.io', // Usually your repo name.
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'ja',
+    locales: ['en','ja'],
   },
 
   presets: [
